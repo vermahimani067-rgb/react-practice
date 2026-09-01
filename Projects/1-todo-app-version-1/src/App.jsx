bdb38e6a4a6f9b24d432331bd8a6ecacd8446892
vermahimani067-rgb <vermahimani067@gmail.com>
2026-09-01 14:38:55 +0000

    update

---
+++ Projects/1-todo-app-version-1/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return <center
+    class='todo-container'>
+    <h1>TODO App</h1>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-4">col-8</div>
+        <div class="col-4">col-8</div>
+        <div class="col-2">col-4</div>
+      </div>
+    </div>
+  </center>
+}
+
+export default App
