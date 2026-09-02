f559b4761d20edf11a5a5a1fb3808ec4f9a5f166
vermahimani067-rgb <vermahimani067@gmail.com>
2026-09-02 08:01:04 +0000

    update

--- Projects/1-todo-app-version-1/src/App.jsx
+++ Projects/1-todo-app-version-1/src/App.jsx
@@ -1,13 +1,23 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./components/TodoItem1";
+
 function App() {
   return <center
     class='todo-container'>
-    <h1>TODO App</h1>
+    <AppName />
+    <AddTodo />
+    <TodoItem1 />
     <div class="container text-center">
+
       <div class="row">
-        <div class="col-4">col-8</div>
-        <div class="col-4">col-8</div>
-        <div class="col-2">col-4</div>
+        <div class="col-4">Go to College</div>
+        <div class="col-4">04/10/2023</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">Delete</button>
+        </div>
       </div>
+
     </div>
   </center>
 }
---
+++ Projects/1-todo-app-version-1/src/components/TodoItem1.jsx
@@ -0,0 +1,13 @@
+function TodoItem1() {
+    return <div class="container text-center">
+        <div class="row">
+            <div class="col-4">Buy Milk</div>
+            <div class="col-4">04/10/2023</div>
+            <div class="col-2">
+                <button type="button" class="btn btn-danger">Delete</button>
+            </div>
+        </div>
+    </div>
+}
+
+export default TodoItem1;
