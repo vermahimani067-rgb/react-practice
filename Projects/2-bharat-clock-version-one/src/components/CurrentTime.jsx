afab083d01154f23a183ec7e07a01236df8d26ae
vermahimani067-rgb <vermahimani067@gmail.com>
2026-09-03 07:51:06 +0000

    update

---
+++ Projects/2-bharat-clock-version-one/src/components/CurrentTime.jsx
@@ -0,0 +1,8 @@
+function CurrentTime() {
+    
+    let time = new Date();
+
+    return <p className="lead">This is the current time: {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>;
+}
+
+export default CurrentTime;
